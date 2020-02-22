bbffe7caf022e6176bc3b239aceb8bee5cb8ac22
Dilan Morar <dilanmorar@gmail.com>
2020-02-22 16:42:42 +0000

    adding python app code and cookbook

---
+++ python_app_cookbook/berks-cookbooks/python_app_cookbook/attributes/default.rb
@@ -0,0 +1,1 @@
+default['github']['repo'] = 'https://github.com/dilanmorar/python_ami'
---
+++ python_app_cookbook/recipes/default.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook:: python_app_cookbook
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+include_recipe node['github']['repo']
